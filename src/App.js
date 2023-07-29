6f8b5a24a06a93b687b1eb58b90276b02592d836
Arathi Nair <arathinair@g.ucla.edu>
2023-07-29 00:13:52 -0700

    add hover feature for officer image bios

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import './App.css';
-import NavigationBar from './Components/NavigationBar';
-import Footer from './Components/Footer';
+import "./App.css";
+import NavigationBar from "./Components/NavigationBar";
+import Footer from "./Components/Footer";
 import Home from "./Pages/Home";
-import {BrowserRouter as Router, Route,Routes } from "react-router-dom";
+import About from "./Pages/About";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App">
-    <Router>
-      <NavigationBar />
-      <Routes>
-        <Route path='/' exact element={<Home />}/>
-      </Routes>
-      <Footer />
-    </Router>
-  </div>;
-
+  return (
+    <div className="App">
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/aboutus" exact element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/About.js
@@ -0,0 +1,165 @@
+import React from "react";
+import "../CSS/About.css";
+// import Image from "../Media/bgpic.JPG";
+
+function About() {
+  return (
+    <div>
+      <div className="bar">
+        <h1> About Us </h1>
+        <h2> Our Mission </h2>
+        <p> This is our mission. Blah blah more blah.</p>
+        <h2> Meet Our Team</h2>
+      </div>
+      <div className="people">
+        <figure>
+          <div className="container">
+            <img src="http://placehold.it/200x200" alt="missing" />
+            <div className="overlay">
+              <div className="text">Bio</div>
+            </div>
+          </div>
+          <figcaption>
+            Suraj Shah <br /> President
+          </figcaption>
+        </figure>
+
+        <figure>
+          <div className="container">
+            <img src="http://placehold.it/200x200" alt="missing" />
+            <div className="overlay">
+              <div className="text">Bio</div>
+            </div>
+          </div>
+          <figcaption>
+            Ella Molyneux <br /> Vice President
+          </figcaption>
+        </figure>
+
+        <figure>
+          <div className="container">
+            <img src="http://placehold.it/200x200" alt="missing" />
+            <div className="overlay">
+              <div className="text">Bio</div>
+            </div>
+          </div>
+          <figcaption>
+            Name <br /> Position
+          </figcaption>
+        </figure>
+
+        <figure>
+          <div className="container">
+            <img src="http://placehold.it/200x200" alt="missing" />
+            <div className="overlay">
+              <div className="text">Bio</div>
+            </div>
+          </div>
+          <figcaption>
+            Name <br /> Position
+          </figcaption>
+        </figure>
+      </div>
+      <div className="people">
+        <figure>
+          <div className="container">
+            <img src="http://placehold.it/200x200" alt="missing" />
+            <div className="overlay">
+              <div className="text">Bio</div>
+            </div>
+          </div>
+          <figcaption>
+            Name <br /> Position
+          </figcaption>
+        </figure>
+
+        <figure>
+          <div className="container">
+            <img src="http://placehold.it/200x200" alt="missing" />
+            <div className="overlay">
+              <div className="text">Bio</div>
+            </div>
+          </div>
+          <figcaption>
+            Name <br /> Position
+          </figcaption>
+        </figure>
+
+        <figure>
+          <div className="container">
+            <img src="http://placehold.it/200x200" alt="missing" />
+            <div className="overlay">
+              <div className="text">Bio</div>
+            </div>
+          </div>
+          <figcaption>
+            Name <br /> Position
+          </figcaption>
+        </figure>
+
+        <figure>
+          <div className="container">
+            <img src="http://placehold.it/200x200" alt="missing" />
+            <div className="overlay">
+              <div className="text">Bio</div>
+            </div>
+          </div>
+          <figcaption>
+            Name <br /> Position
+          </figcaption>
+        </figure>
+      </div>
+      <div className="people">
+        <figure>
+          <div className="container">
+            <img src="http://placehold.it/200x200" alt="missing" />
+            <div className="overlay">
+              <div className="text">Bio</div>
+            </div>
+          </div>
+          <figcaption>
+            Name <br /> Position
+          </figcaption>
+        </figure>
+
+        <figure>
+          <div className="container">
+            <img src="http://placehold.it/200x200" alt="missing" />
+            <div className="overlay">
+              <div className="text">Bio</div>
+            </div>
+          </div>
+          <figcaption>
+            Name <br /> Position
+          </figcaption>
+        </figure>
+
+        <figure>
+          <div className="container">
+            <img src="http://placehold.it/200x200" alt="missing" />
+            <div className="overlay">
+              <div className="text">Bio</div>
+            </div>
+          </div>
+          <figcaption>
+            Name <br /> Position
+          </figcaption>
+        </figure>
+
+        <figure>
+          <div className="container">
+            <img src="http://placehold.it/200x200" alt="missing" />
+            <div className="overlay">
+              <div className="text">Bio</div>
+            </div>
+          </div>
+          <figcaption>
+            Name <br /> Position
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+  );
+}
+
+export default About;
